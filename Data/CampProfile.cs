0430ce94625f13c93337b8314e443a80a52d3c9c
MaciejBiernat <57531118+MaciejBiernat@users.noreply.github.com>
2020-09-02 14:23:48 +0200

    Add project files.

---
+++ Data/CampProfile.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CoreCodeCamp.Data
+{
+    public class CampProfile : Profile
+    {
+        public CampProfile()
+        {
+
+            // TODO to trzeba zgłębić!!!
+            //this.CreateMap<Camp, CampModel>()
+            //    .ForMember(c => c.Venue, o=> o.MapFrom(m => m.Location.VenueName));
+
+            this.CreateMap < Camp, CampModel> ()
+                .ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName))
+                .ReverseMap();
+
+            this.CreateMap<Talk, TalkModel>()
+                .ReverseMap()
+                .ForMember(t => t.Camp, opt => opt.Ignore())
+                .ForMember(t => t.Speaker, opt=> opt.Ignore());
+
+            this.CreateMap<Speaker, SpeakerModel>()
+                .ReverseMap();
+        }
+    }
+}
